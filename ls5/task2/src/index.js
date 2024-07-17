7ffaca8486101a85f95a180e3d4e63112aaf2399
macakklett <macanvitalijj@gmail.com>
2024-07-17 20:45:31 +0300

    ls5-2

---
+++ ls5/task2/src/index.js
@@ -0,0 +1,10 @@
+import { fetchUser } from './profile/gateway.js';
+// import { printProfile } from './profile/index.js';
+import printProfile from './profile/printProfile';
+
+fetchUser('github').then((userData) =>
+  printProfile({
+    name: userData.name,
+    company: userData.location,
+  })
+);
---
+++ ls5/task2/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  entry: {
+    profile: './src/profile/index.js',
+    dashboard: './src/dashboard/index.js',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, 'build'),
+  },
+  mode: 'production',
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+};
