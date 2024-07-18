0e5c775650796d96aec98d9a6d84522c6a9ca951
macakklett <macanvitalijj@gmail.com>
2024-07-18 12:01:55 +0300

    ls6-1

---
+++ ls6/task1/webpack.config.js
@@ -0,0 +1,36 @@
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+
+  output: {
+    filename: 'bundle.js',
+  },
+
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /.s?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /.(jpg|png)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              name: '[name].[ext]',
+              outputPath: 'images',
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
