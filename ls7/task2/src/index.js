2bf46844798a41e4e61f17676a036a3e324d8e88
macakklett <macanvitalijj@gmail.com>
2024-07-19 11:00:44 +0300

    ls7-2

---
+++ ls7/task2/src/index.js
@@ -0,0 +1,33 @@
+import { renderTasks } from './scripts/renderTasks.js';
+import { onAddTask } from './scripts/addTask.js';
+import { getTasks } from './scripts/tasksGateway.js';
+import { changeDone } from './scripts/changeDone.js';
+import { deleteTask } from './scripts/deleteTask.js';
+import './index.scss';
+
+const list = document.querySelector('.list');
+const createTaskBtn = document.querySelector('.create-task-btn');
+
+getTasks().then((response) => renderTasks(response));
+
+const onListHandler = (e) => {
+  if (e.target.classList.contains('list-item__checkbox')) {
+    const task = {
+      id: e.target.closest('.list-item').dataset.id,
+      done: e.target.checked,
+      finishDate: e.target.checked ? new Date() : null,
+    };
+
+    changeDone(task);
+  }
+
+  if (e.target.classList.contains('list-item__close')) {
+    const id = e.target.closest('.list-item').dataset.id;
+    deleteTask(id);
+  }
+
+  return;
+};
+
+list.addEventListener('click', onListHandler);
+createTaskBtn.addEventListener('click', onAddTask);
